f5edeb8eb3a8910c3901bb615f42a6512f8cc92b
Antoni Rasul <antoni.rasul@gmail.com>
2025-02-16 10:54:05 +0100

    Perform concurrent requests in chunks to avoid hitting any concurrent request limits of the NASA API.

--- src/main/kotlin/com/harper/asteroids/client/NasaApiClient.kt
+++ src/main/kotlin/com/harper/asteroids/client/NasaApiClient.kt
@@ -16,13 +16,14 @@ class NasaApiClient(
     private val apiKey: String = NasaApiConfig.apiKey
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
-    private val API_URL = "https://api.nasa.gov"
-    private val NEO_FEED_URL = "$API_URL/neo/rest/v1/feed"
-    private val NEO_LOOKUP_URL = "$API_URL/neo/rest/v1/neo"
+    private val neoApiUrl = "https://api.nasa.gov/neo/rest/v1"
+    private val feedUrl = "$neoApiUrl/feed"
+    private val lookupUrl = "$neoApiUrl/neo"
+    private val concurrentRequestsCount = 8
     
     fun getNeoFeed(startDate: LocalDate, endDate: LocalDate): Feed = runBlocking {
         log.info("Getting NEO feed from $startDate till $endDate")
-        httpClient.get(NEO_FEED_URL) {
+        httpClient.get(feedUrl) {
             parameter("start_date", startDate)
             parameter("end_date", endDate)
             parameter("api_key", apiKey)
@@ -30,16 +31,20 @@ class NasaApiClient(
     }
     
     fun lookupByIds(asteroidIds: List<Int>): List<NearEarthObject> = runBlocking {
-        asteroidIds
-            .map { asteroidId ->
-                async { lookupById(asteroidId) } }
-            .toList()
-            .awaitAll()
+        log.info("Looking up ${asteroidIds.size} asteroids in concurrent chunks of size $concurrentRequestsCount")
+        asteroidIds.chunked(concurrentRequestsCount)
+            .mapIndexed{ idx, asteroidIdsChunk ->
+                log.info("Fetching chunk $idx for ids: $asteroidIdsChunk")
+                asteroidIdsChunk
+                    .map { asteroidId -> async { lookupById(asteroidId) } }
+                    .toList()
+                    .awaitAll()
+            }.flatten()
     }
     
     private suspend fun lookupById(asteroidId: Int): NearEarthObject {
-        log.info("Check passing of object $asteroidId")
-        return httpClient.get("$NEO_LOOKUP_URL/$asteroidId") {
+        log.debug("Check passing of object $asteroidId")
+        return httpClient.get("$lookupUrl/$asteroidId") {
             parameter("api_key", apiKey)
         }.body()
     }
